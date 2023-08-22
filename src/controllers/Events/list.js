d4754c9b9fffbb06b042b197a2819b7787a602b6
ivoherre <i.herrera98@hotmail.com>
2023-08-22 11:41:32 -0400

    completed joined and event

--- src/controllers/Events/list.js
+++ src/controllers/Events/list.js
@@ -2,7 +2,7 @@ const list = async (req, res) => {
     const {
       // session, // this req.session property is put here by the handleCookieSessions middleware
       db: { Event }, // this req.db.User property is put here by the addModelsToRequest middleware
-      body: { }, // this req.body property is put here by the client
+      // body: { }, // this req.body property is put here by the client
     } = req;
   
     // TODO: check if username is taken, what should you return?
--- src/db/models/Joined.js
+++ src/db/models/Joined.js
@@ -1,29 +1,38 @@
 const knex = require('../knex');
 
 class Joined{
-static async create(){
+static async create(user_id, event_id){
     try{
+       const query = 'INSERT INTO joined (user_id, event_id) VALUES(?,?)'
+       const {rows: [joined]} = await knex.raw(query, [user_id, event_id])
+       return joined
     }
     catch(error){
         console.log(error)
         return null
     }
 }
-static async show(){
+static async show(user_id){
     try{
+       const query = 'SELECT * FROM joined WHERE user_id=?'
+       const {rows: [list]} = knex.raw(query, [user_id])
+       return list
     }
     catch(error){
         console.log(error)
         return null
     }
 }
-static async delete(){
-    try{
+static async delete(id, user_id) {
+    try {
+        const query = "DELETE FROM joined WHERE id=? AND user_id = ? RETURNING *"
+        const {rows: [joined]} = await knex.raw(query,[id, user_id])
+        return joined
     }
-    catch(error){
-        console.log(error)
-        return null
-    }
-}
+    catch(error) {
+        console.log('ERROR!', error); 
+        return null;
+    } 
+} 
 }
 module.exports = Joined;
